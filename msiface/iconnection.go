9123fc70c23c825d3258438bd7449d9f7ca2143a
undefinedot <1055821119@qq.com>
2022-06-13 04:49:47 +0800

    delete type HandleFunc

--- msiface/iconnection.go
+++ msiface/iconnection.go
@@ -11,7 +11,3 @@ type IConnection interface {
 	RemoteAddr() net.Addr // 远程客户端的Addr
 	Send(data []byte) error // 发数据给远程客户端
 }
-
-// HandleFunc 定义一个处理连接业务的方法，函数类型
-// TODO: 带更改, p6:11:55
-type HandleFunc func(*net.TCPConn, []byte, int) error
